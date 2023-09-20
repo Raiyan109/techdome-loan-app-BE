dd3ba0f311877157e819a7a47200741ca3e861de
Raiyan109 <kraiyan109@gmail.com>
2023-09-20 17:40:00 +0600

    signup, login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModels");
+const bcrypt = require('bcrypt');
 
 // GET all users
 const getAllUsers = async (req, res, next) => {
@@ -15,6 +16,69 @@ const getAllUsers = async (req, res, next) => {
     return res.status(200).json({ users })
 }
 
+// Signup
+const signup = async (req, res) => {
+    const { name, email, password } = req.body
+
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email })
+    } catch (error) {
+        console.log(error);
+    }
+
+    if (existingUser) {
+        return res.status(400).json({ message: 'User already signup. try login instead' })
+    }
+
+    // Bcrypt
+
+    // const salt = bcrypt.genSaltSync(saltRounds);
+    const saltRounds = 10
+    const hashedPassword = bcrypt.hashSync(password, saltRounds)
+
+    const user = new User({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    try {
+        await user.save()
+    } catch (error) {
+        console.log(error);
+    }
+
+    return res.status(200).json({ user })
+}
+
+// Login POST
+const login = async (req, res) => {
+    const { email, password } = req.body
+
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email })
+    } catch (error) {
+        console.log(error);
+    }
+
+    if (!existingUser) {
+        return res.status(400).json({ message: "User not found with this email" })
+    }
+
+    // Bcrypt compare password
+    const isCorrectPassword = bcrypt.compareSync(password, existingUser.password)
+
+    if (!isCorrectPassword) {
+        return res.status(400).json({ message: 'Password did not match' })
+    }
+
+    return res.status(200).json({ message: "Login successful" })
+}
+
 module.exports = {
     getAllUsers,
+    signup,
+    login,
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { getAllUsers } = require('../controllers/userControllers');
+const { getAllUsers, signup, login } = require('../controllers/userControllers');
 
 const router = express.Router()
 
 router.get('/', getAllUsers)
+
+// Signup
+router.post('/signup', signup)
+
+// Login
+router.post('/login', login)
 
 module.exports = router
 
